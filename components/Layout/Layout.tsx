342bb641bbcaccc81b17ec43a782a3761565792c
Alex Zhukov <aleksandr.a.zhukov@gmail.com>
2022-06-27 19:08:42 +0300

    added chakra ui and simple layout

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+import { PropsWithChildren } from 'react';
+import React from 'react';
+
+import { Footer } from '../Footer';
+import { Header } from '../Header';
+
+export const Layout = ({ children }: PropsWithChildren) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import type { AppProps } from 'next/app';
 
-import '../styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
+
+import { Layout } from '../components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
