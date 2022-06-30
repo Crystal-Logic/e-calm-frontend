5e78bab88a39f10d905f63dafd151e3a1edc8426
Alex Zhukov <aleksandr.a.zhukov@gmail.com>
2022-06-30 11:07:31 +0300

    moved contacts to component

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,33 +8,8 @@ import field from '../assets/field.jpg';
 import hands from '../assets/hands.jpg';
 import ECalmLogoWhite from '../assets/icons/e-calm-logo-white.svg';
 import TridentIcon from '../assets/icons/trident.svg';
+import { Contacts } from '../components/Contacts';
 import { FAQ } from '../components/FAQ';
-
-type Contact = {
-  title: string;
-  subTitle?: string;
-  phones: string[];
-};
-
-const ContactItem = ({ title, subTitle, phones }: Contact) => (
-  <Box key={title}>
-    <Heading as="h3" maxWidth={400} mb={5} flex={1} size={{ base: 'sm', md: 'lg' }}>
-      {title}
-    </Heading>
-    {subTitle && (
-      <Text size={{ base: 'md', md: 'lg' }} mb={5}>
-        {subTitle}
-      </Text>
-    )}
-    {phones.map((phone, index) => (
-      <Link display="block" mb={2} key={index} href={`tel:${phone}`}>
-        <Heading as="span" size={{ base: 'xs', md: 'sm' }}>
-          {phone}
-        </Heading>
-      </Link>
-    ))}
-  </Box>
-);
 
 const Home: NextPage = () => {
   const { t } = useTranslation('common');
@@ -118,41 +93,7 @@ const Home: NextPage = () => {
         </Heading>
       </Container>
 
-      <Box bg="#F6F6FA">
-        <Container py={{ base: 16, md: 16 }}>
-          <Heading as="h2" mb={12} size={{ base: 'md', md: '2xl' }}>
-            {t('contacts.title')}
-          </Heading>
-          <Flex direction={{ base: 'column', md: 'row' }} gap={8}>
-            <Flex
-              flex={1}
-              borderRadius="20px"
-              bg="rgba(255, 255, 255, 0.41)"
-              px={{ base: 4, md: 8 }}
-              py={{ base: 8, md: 12 }}
-              gap={{ base: 8, md: 16 }}
-              flexDirection="column"
-            >
-              {(t('contacts.left', { returnObjects: true }) as Contact[]).map((contact) => (
-                <ContactItem {...contact} key={contact.title} />
-              ))}
-            </Flex>
-            <Flex
-              flex={1}
-              borderRadius="20px"
-              bg="rgba(255, 255, 255, 0.41)"
-              px={{ base: 4, md: 8 }}
-              py={{ base: 8, md: 12 }}
-              gap={{ base: 8, md: 16 }}
-              flexDirection="column"
-            >
-              {(t('contacts.right', { returnObjects: true }) as Contact[]).map((contact) => (
-                <ContactItem {...contact} key={contact.title} />
-              ))}
-            </Flex>
-          </Flex>
-        </Container>
-      </Box>
+      <Contacts />
 
       <Box
         backgroundImage={`url(${field.src})`}
