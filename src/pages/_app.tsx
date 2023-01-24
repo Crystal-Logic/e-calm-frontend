22208565e1ff7ac1c1fd83c6d720c5a72ba6b606
amostap <alexandr.amostap@gmail.com>
2023-01-24 19:15:13 +0300

    Add netlify headers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -57,6 +57,7 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#fff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <link rel="apple-touch-icon" href="/icons/icon-512x512.png" />
         <link rel="apple-touch-icon" sizes="152x152" href="/icons/icon-152x152.png" />
